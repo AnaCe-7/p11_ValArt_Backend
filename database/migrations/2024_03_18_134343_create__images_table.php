7d82d96a09b9c3dbe966b7bc81f5e6088930e8ae
AnaCe-7 <ana.cecilia.reques@gmail.com>
2024-03-18 14:45:06 +0100

    Create Images migration(2)

---
+++ database/migrations/2024_03_18_134343_create__images_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('_images', function (Blueprint $table) {
+            $table->id();
+            $table->string('image_url');
+            $table->string('public_id');
+            $table->unsignedBigInteger('artwork_id');
+            $table->timestamps();
+
+            $table->foreign('artwork_id')->references('id')->on('artworks')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('_images');
+    }
+};
