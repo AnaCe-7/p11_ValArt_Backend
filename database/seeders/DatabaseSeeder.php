acb8a7cd626621c307909ec392b8ecad44c652a4
AnaCe-7 <ana.cecilia.reques@gmail.com>
2024-03-18 21:12:55 +0100

    Update DatabaseSeeder(2)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $this->call([
+            ArtworkSeeder::class,
+            ImageSeeder::class,
+        ]);
     }
 }
