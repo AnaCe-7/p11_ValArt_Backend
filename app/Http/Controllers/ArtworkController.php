d67ca457df7ecf21b9ddc1327a50f24503b2a541
AnaCe-7 <ana.cecilia.reques@gmail.com>
2024-03-19 01:22:01 +0100

    Create store function

--- app/Http/Controllers/ArtworkController.php
+++ app/Http/Controllers/ArtworkController.php
@@ -14,4 +14,28 @@ class ArtworkController extends Controller
         return response()->json($artworks, 200);
     }
 
+    public function store(Request $request)
+    {
+        try {
+
+            $artwork = new Artwork;
+            $artwork -> title = $request -> title;
+            $artwork -> description = $request -> description;
+            $artwork -> image = $request -> image;
+            $artwork -> classification = $request -> classification;
+            $artwork -> technique = $request -> technique;
+            $artwork -> details = $request -> details;
+            $artwork -> measures = $request -> measures;
+            $artwork->save();
+            
+            return response()->json([
+                'success' => true,
+                'data' => $artwork
+            ], 201);
+    
+            } catch(\Exception $e){
+                return response()->json(['error' => 'Failed to store artwork'], 500);
+            }
+    }
+
 }
