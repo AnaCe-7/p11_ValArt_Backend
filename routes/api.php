166768431e7fe1eff9e52caa98c5c94dc2f8385d
AnaCe-7 <ana.cecilia.reques@gmail.com>
2024-03-20 12:35:53 +0100

    Create ImageController Routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ArtworkController;
+use App\Http\Controllers\ImageController;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -26,4 +27,12 @@ Route::group([], function(){
     Route::get('/artwork/{id}', [ArtworkController::class, 'show']);
     Route::put('/artwork/{id}', [ArtworkController::class, 'update']);
     Route::delete('/artwork/{id}', [ArtworkController::class, 'destroy']);
+});
+
+Route::group([], function(){
+    Route::get('/', [ImageController::class, 'index']);
+    Route::post('/artwork', [ImageController::class, 'store']);
+    Route::get('/artwork/{id}', [ImageController::class, 'show']);
+    Route::put('/artwork/{id}', [ImageController::class, 'update']);
+    Route::delete('/artwork/{id}', [ImageController::class, 'destroy']);
 });
